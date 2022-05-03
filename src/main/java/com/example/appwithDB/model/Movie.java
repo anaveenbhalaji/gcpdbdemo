1dc32b01d0bb20745e08dc64392b009f0af74d29
NaveenBhalaji <anaveenbhalaji@gmail.com>
2022-05-03 12:18:56 -0500

    new changes fix

--- src/main/java/com/example/appwithDB/model/Movie.java
+++ src/main/java/com/example/appwithDB/model/Movie.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Table
 public class Movie {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String genre;
